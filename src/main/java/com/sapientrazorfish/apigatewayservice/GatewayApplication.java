b5174b4ec9db45ec634e25059bff3274a1476df9
Srinivasan Narayanan <snarayanan@sapient.com>
2017-09-27 19:16:48 +0530

    Fixing build issues

--- src/main/java/com/sapientrazorfish/apigatewayservice/GatewayApplication.java
+++ src/main/java/com/sapientrazorfish/apigatewayservice/GatewayApplication.java
@@ -1,5 +1,8 @@
 package com.sapientrazorfish.apigatewayservice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,11 +13,11 @@ import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProper
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
@@ -30,29 +33,31 @@ import com.sapientrazorfish.apigatewayservice.filters.pre.SimpleFilter;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
-	
+
 	@Autowired
 	private OAuth2ClientProperties clientProperties;
-	
+
 	@Autowired
 	private ResourceServerProperties resourceProperties;
 
-  public static void main(String[] args) {
-    SpringApplication.run(GatewayApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 
-  @Bean
-  public SimpleFilter simpleFilter() {
-    return new SimpleFilter();
-  }
-  
-  @Bean
-	UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+	@Bean
+	UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(LoadBalancerInterceptor loadBalancerInterceptor) {
 		return template -> {
+			List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+			interceptors.add(loadBalancerInterceptor);
 			AccessTokenProviderChain accessTokenProviderChain = Stream
 					.of(new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
 							new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider())
-					.peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+					.peek(tp -> tp.setInterceptors(interceptors))
 					.collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
 			template.setAccessTokenProvider(accessTokenProviderChain);
 		};
@@ -61,9 +66,9 @@ public class GatewayApplication {
 	@Bean
 	@Primary
 	ResourceServerTokenServices resourceServerTokenServices(
-			SpringClientFactory springClientFactory) {
+			LoadBalancerInterceptor loadBalancerInterceptor) {
 		RestTemplate tokenRest = new RestTemplate();
-		tokenRest.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory));
+		tokenRest.setInterceptors(Arrays.asList(loadBalancerInterceptor));
 		RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
 		remoteTokenServices.setRestTemplate(tokenRest);
 		remoteTokenServices.setClientId(clientProperties.getClientId());
